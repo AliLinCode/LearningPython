41c9c5be21d70e6a56a16816bd290df019933a33
alex <oleksandr.linchuk@busmarket.ua>
2024-08-01 16:07:13 +0300

    Add Module 2 lab

---
+++ Module2/Lab/2_6_11LAB   Operators and expressions – 2 /2_6_11.py
@@ -0,0 +1,13 @@
+hour = int(input("Starting time (hours): "))
+mins = int(input("Starting time (minutes): "))
+dura = int(input("Event duration (minutes): "))
+
+mins = mins + dura # find a total of all minutes
+print(mins)
+hour = hour + mins // 60 # find a number of hours hidden in minutes and update the hour
+print(hour)
+mins = mins % 60 # correct minutes to fall in the (0..59) range
+print(mins)
+hour = hour % 24 # correct hours to fall in the (0..23) range
+print(mins)
+print(hour, ":", mins, sep='')
