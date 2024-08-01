4912d3dad229abf7ff8006dc48ef7de4754d29b6
alex <oleksandr.linchuk@busmarket.ua>
2024-08-01 15:22:13 +0300

    Add Module 2 lab

---
+++ Module2/Lab/2_6_9   LAB   Simple input and output /2_6_9.py
@@ -0,0 +1,16 @@
+# input a float value for variable a here
+print("input a float value for variable a here:  ")
+a=float(input())
+# input a float value for variable b here
+print ("Input a float value for variable b here")
+b=float(input())
+# output the result of addition here
+print(a+b)
+# output the result of subtraction here
+print(a-b)
+# output the result of multiplication here
+print(a*b)
+# output the result of division here
+print(a/b)
+print("\nThat's all, folks!")
+
