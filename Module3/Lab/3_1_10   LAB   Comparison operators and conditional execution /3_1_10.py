ee0e24a05b2b6bb8ca6fb8c36de780427a9bb9af
alex <oleksandr.linchuk@busmarket.ua>
2024-08-02 09:42:10 +0300

    Add Module 2 lab

---
+++ Module3/Lab/3_1_10   LAB   Comparison operators and conditional execution /3_1_10.py
@@ -0,0 +1,8 @@
+plant_name=(input("Введіть  назву рослини:  "))
+if plant_name == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+elif plant_name == "spathiphyllum":
+    print("No, I want a big Spathiphyllum!")
+else:
+    print("Spathiphyllum! Not ", plant_name + "!")
+
