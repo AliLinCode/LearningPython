c9d229d6195fcd9d980d019dda0dc3fee6a378c5
alex <oleksandr.linchuk@busmarket.ua>
2024-08-02 16:13:58 +0300

    Add 3_2_15 lab Original Solution

--- Module3/Lab/3_2_15   LAB   Collatz's hypothesis /3_2_15.py
+++ Module3/Lab/3_2_15   LAB   Collatz's hypothesis /3_2_15.py
@@ -10,14 +10,14 @@ else:
         if c0%2 == 0:
             print("if 1")
             c0 = c0 / 2
-            print(c0)
+            print(int(c0))
             steps += 1
-            time.sleep(3)
+
 
         elif c0%2 == 1:
             print("if 2")
             c0 = 3 * c0 + 1
-            print(c0)
+            print(int(c0))
             steps += 1
 
 print("Steps: ", steps)
