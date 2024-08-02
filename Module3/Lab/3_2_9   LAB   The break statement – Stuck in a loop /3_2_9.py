c2abbf9ad4d18846ab9c6866f65f7bcdd0656a8c
alex <oleksandr.linchuk@busmarket.ua>
2024-08-02 13:14:03 +0300

    Add 3_2_9 lab Original Solution

---
+++ Module3/Lab/3_2_9   LAB   The break statement – Stuck in a loop /3_2_9.py
@@ -0,0 +1,7 @@
+while True:
+    word = input("Entrer your word: ")
+    if word == "chupacabra":
+        print("You've successfully left the loop")
+        break
+
+
--- Module3/test_code.py
+++ Module3/test_code.py
@@ -1,2 +1,18 @@
-for i in range(1,1):
-    print("The value of i is currently", i)
+# break - example
+
+print("The break instruction:")
+for i in range(1, 6):
+    if i == 3:
+        break
+    print("Inside the loop.", i)
+print("Outside the loop.")
+
+
+# continue - example
+
+print("\nThe continue instruction:")
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print("Inside the loop.", i)
+print("Outside the loop.")
