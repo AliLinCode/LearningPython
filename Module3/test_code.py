4e659b69c8e0f4c6025837e2022f4b83f59f0226
alex <oleksandr.linchuk@busmarket.ua>
2024-08-05 15:31:46 +0300

    Add 4_3_4 lab Original Solution

--- Module3/test_code.py
+++ Module3/test_code.py
@@ -1,8 +1,10 @@
-my_list = [17, 3, 11, 5, 1, 9, 7, 15, 13]
-largest = my_list[0]
+def message():
+    print("Enter a value: ")
 
-for i in range(1, len(my_list)):
-    if my_list[i] > largest:
-        largest = my_list[i]
 
-print(largest)
+message()
+a = int(input())
+message()
+b = int(input())
+message()
+c = int(input())
