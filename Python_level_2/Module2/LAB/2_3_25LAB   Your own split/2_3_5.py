b8e89dc445e9eea5dc651b818215e1e647c5e4e0
alex <oleksandr.linchuk@busmarket.ua>
2024-08-08 17:10:16 +0300

    Module 2 Level2 add 2_3_5Lab

--- Python_level_2/Module2/LAB/2_3_25LAB   Your own split/2_3_5.py
+++ Python_level_2/Module2/LAB/2_3_25LAB   Your own split/2_3_5.py
@@ -11,3 +11,38 @@ print(mysplit("To be or not to be,that is the question"))
 print(mysplit("   "))
 print(mysplit(" abc "))
 print(mysplit(""))
+
+
+def mysplit1(strng):
+    if strng == '' or strng.isspace():
+        return [ ]
+    lst = []
+    word = ''
+    inword = not strng[0].isspace()
+    for x in strng:
+        if inword:
+            if not x.isspace():
+                word = word + x
+            else:
+                lst.append(word)
+                inword = False
+        else:
+            if not x.isspace():
+
+                inword = True
+
+                word = x
+            else:
+                pass
+
+    if inword:
+        lst.append(word)
+
+    return lst
+
+
+print(mysplit1("To be or not to be, that is the question"))
+print(mysplit1("To be or not to be,that is the question"))
+print(mysplit1("   "))
+print(mysplit1(" abc "))
+print(mysplit1(""))
