6e45fe76bbcc151bede93ae5c77d51ba434c6a2d
alex <oleksandr.linchuk@busmarket.ua>
2024-08-08 16:52:52 +0300

    Module 2 Level2 add 2_3_5Lab

---
+++ Python_level_2/Module2/LAB/2_3_25LAB   Your own split/2_3_5.py
@@ -0,0 +1,13 @@
+def mysplit(strng):
+   return strng.split()
+
+#
+# put your code here
+#
+
+
+print(mysplit("To be or not to be, that is the question"))
+print(mysplit("To be or not to be,that is the question"))
+print(mysplit("   "))
+print(mysplit(" abc "))
+print(mysplit(""))
--- Python_level_2/Module2/test_code.py
+++ Python_level_2/Module2/test_code.py
@@ -1,13 +1,3 @@
-import gettext
-import locale
-
-# Встановлюємо локаль
-locale.setlocale(locale.LC_ALL, '')
-
-# Встановлюємо каталог з перекладами
-localedir = 'locale'
-lang = gettext.translation('myapp', localedir, languages=['es'])
-lang.install()
-
-# Використовуємо функцію _() для перекладу
-print(_('Hello, World!'))
+the_list = ['Where', 'are', 'the', 'snows?']
+s = '-'.join(the_list)
+print(s)
